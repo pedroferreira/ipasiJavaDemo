0c31f59a6b5b05bd159c2eb8012ce3bd5f84b954
D-Duarte <2160444@my.ipleiria.pt>
2016-10-13 10:21:54 +0100

    Adicionei um nome de Aluno.
    Também acrescentei comentários para nomes mal formados.

--- src/iapsidemo2016/model/ApplicationData.java
+++ src/iapsidemo2016/model/ApplicationData.java
@@ -23,23 +23,21 @@ public class ApplicationData {
     public ApplicationData(){
         
         this.students = new ArrayList<>();
+        this.students.add(new Student("Diogo J. C. Duarte","2160444")); /*O "J.C." é para "Jesus Cristo" se quiserem. */
         this.students.add(new Student("Leonardo Henriques","2160484"));
         this.students.add(new Student("pedro ferreira", "2121212"));
         this.students.add(new Student("ricardo martinho", "2121213"));        
-        this.students.add(new Student("aluno nome", "2111222"));      
+        this.students.add(new Student("aluno nome", "2111222"));    /*LOLWUT*/      
         this.students.add(new Student("carlos pedroso", "2161728"));
         this.students.add(new Student("Rui Simões", "2160481"));    
         this.students.add(new Student("luis morgado", "2160435")); 
         this.students.add(new Student("Alexandre Pereira", "2160479"));
-        
-
         this.students.add(new Student("ricardo martinho", "2121213"));        
-        this.students.add(new Student("aluno nome", "21112  22"));  
+        this.students.add(new Student("aluno nome", "21112  22"));      /*Está pior!*/  
         this.students.add(new Student("Francisco Ronsani", "2160477"));
         this.students.add(new Student("André Vicente", "2160430"));
         this.students.add(new Student("Rafael Batista", "2160432"));
         this.students.add(new Student("Rúben Rosa", "2160438"));
-        this.students.add(new Student("Ricardo Fernandes", "2160448"))
     }
 
     public ArrayList<Student> getStudents() {

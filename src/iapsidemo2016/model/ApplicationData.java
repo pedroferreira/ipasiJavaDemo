d23c51f391d95411e92ee6237d30f1c69c3a1e18
2160457 <2160457@C2-LBD-M02.IPLeiria.pt>
2016-10-13 10:42:07 +0100

    Adicionei um novo estudante

--- src/iapsidemo2016/model/ApplicationData.java
+++ src/iapsidemo2016/model/ApplicationData.java
@@ -39,6 +39,7 @@ public class ApplicationData {
         this.students.add(new Student("Rafael Batista", "2160432"));
         this.students.add(new Student("Rúben Rosa", "2160438"));
         this.students.add(new Student("new", "rwo"));
+        this.students.add(new Student("Cristiano Luis", "2161725"));
         
     }
 

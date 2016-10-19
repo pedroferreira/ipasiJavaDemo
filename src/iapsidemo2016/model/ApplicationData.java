12a74bba39959f12e47ad3ede067c8b8a317c223
João Vieira <Joo Vieira@192.168.133.13>
2016-10-19 16:12:26 +0100

    adicionei um novo estudante

--- src/iapsidemo2016/model/ApplicationData.java
+++ src/iapsidemo2016/model/ApplicationData.java
@@ -28,7 +28,7 @@ public class ApplicationData {
         this.students.add(new Student("bruno leitão", "2160449")); 
         this.students.add(new Student("diogo carvalho", "2160454")); 
         this.students.add(new Student("andré paulo", "2160446")); 
-        this.students.add(new Student("João Vieira", "2160451"));
+        this.students.add(new Student("andré paulo", "2160446"));
     }
 
     public ArrayList<Student> getStudents() {

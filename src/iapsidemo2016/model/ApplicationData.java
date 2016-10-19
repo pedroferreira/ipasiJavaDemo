774fd57df261334b1b30505ea0e980afd96bbf5b
vicen <vicen@192.168.237.221>
2016-10-19 13:10:46 +0100

    adicionei um novo estudante

--- src/iapsidemo2016/model/ApplicationData.java
+++ src/iapsidemo2016/model/ApplicationData.java
@@ -27,6 +27,8 @@ public class ApplicationData {
         this.students.add(new Student("Vicente Severino", "2160452"));
         this.students.add(new Student("joao porelo", "2160453"));
         this.students.add(new Student ("aluno 2160472", "2222222"));
+       
+        this.students.add(new Student("diogo dias","2160440"));
     }
 
     public ArrayList<Student> getStudents() {
